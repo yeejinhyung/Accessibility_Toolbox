1fe32fbc1ddd42ee419b13376565498e3dc05ff7
yeejinhyung <71398384+yeejinhyung@users.noreply.github.com>
2021-05-25 15:45:59 -0400

    updates

---
+++ rmd2jupyter.R
@@ -0,0 +1,6 @@
+
+## install and load rmd2jupyter
+devtools::install_github("mkearney/rmd2jupyter")
+
+library(rmd2jupyter)
+rmd2jupyter::rmd2jupyter("/Users/jinlee/GitHub/Accessibility_Toolbox/Accessibility_Calculator_for_R.Rmd")
